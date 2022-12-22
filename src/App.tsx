58afcd513f76e31fd060d56e7bed45d32fb2b44c
kawsar.dev <kawsarmia@tetonelectronics.com>
2022-12-22 09:53:37 -0800

    Initial commit setup tailwind css

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="bg-black text-white" >
+     Hello Typescript
+    </div>
+  )
+}
+
+export default App
