d530dcf019104bc84403b81ef3aaa3991684b541
kawsar.dev <kawsarmia@tetonelectronics.com>
2022-12-24 06:51:19 -0800

    Create Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Navbar from './scenes/navbar'
 
 function App() {
+  const [selectedPage, setSelectedPage] = useState<string>("home");
 
   return (
-    <div className="bg-black text-white" >
-     Hello Typescript
+    <div className="app bg-gray-20">
+     <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
     </div>
   )
 }
