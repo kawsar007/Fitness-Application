2b5f5f57a8e86bc0af5484f3c3e367f466f27172
kawsar.dev <kawsarmia@tetonelectronics.com>
2022-12-26 05:59:43 -0800

    Create Home Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import './App.css'
+import Home from './scenes/home';
 import Navbar from './scenes/navbar'
 import { SelectedPage } from './shared/types';
 
@@ -22,6 +23,7 @@ function App() {
   return (
     <div className="app bg-gray-20">
       <Navbar isTopOfPage={isTopOfPage} selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+      <Home setSelectedPage={setSelectedPage}/>
     </div>
   )
 }
